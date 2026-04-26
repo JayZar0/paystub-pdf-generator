268e0304868b36ef38cc4257cfc3e9d7fecca9d6
John L <lazaro4958@saskpolytech.ca>
2026-04-26 13:13:27 -0600

    pdf generator function implementations

--- src/main.py
+++ src/main.py
@@ -1,28 +1,33 @@
 # This file is the main code for the application
 import requests
 import os
+from datetime import date
 from dotenv import load_dotenv
 from pdfgen import generator
 
-# Grab the api key
+# Grab the api key, then place it in the request header and also grab the url
 load_dotenv()
 api_key = os.getenv('API_KEY')
 url = os.getenv('PAYROLL_URL')
-
-# Perform the webrequest
 headers={
   "Authorization": f"Bearer ${api_key}",
   "accept":"application/json"
 }
+
+
+# Perform the webrequest
 res = requests.get(url, headers=headers)
 
 if res.ok:
   # If the response is good then generate the pdf file using the elements in the
   # web pay stub
-  print("The request works")
+  print(f"Request succeeded on ${date.today()}")
   print(res.json())
   pdf = generator()
+  pdf.generatePdf(res.json())
 
-  # after generating the pdf file then save it in the 
+  # after generating the pdf file then save it in the
+  pdf.savePdf('./') 
 else:
-  print("The request has failed or your api key is invalid")
+  # TODO: create other errors based on where generation failed
+  print(f"The request has failed on ${date.today()}")
--- src/pdfgen.py
+++ src/pdfgen.py
@@ -5,3 +5,12 @@ class generator:
     self.filename = filename
     pass
 
+  # This function will be used to generate a pdf file with all the data passed
+  # into the constructor
+  def generatePdf(paydata):
+    raise NotImplementedError("Function currently not implemented")
+
+  # This function will be used to save the pdf file to the passed in directory
+  def savePdf(directory):
+    raise NotImplementedError("Function currently not implemented")
+
